373c66aa6294b76004113973d55b10ee83daf05b
Emily Rocha <101346141+Rochaem34@users.noreply.github.com>
2024-01-24 13:56:30 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Emily Rocha')
+print(1, 'Emily Rocha"')
+
+# Aspas duplas
+print("Emily Rocha")
+print(2, "Emily Rocha'")
+
+# Escape
+print("Emily \"Rocha\"")
+
+# r
+print(r"Emily Rocha\"")
